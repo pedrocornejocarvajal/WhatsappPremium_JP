1e966749deb1360fc52ad764fca4d448e7b8b7ee
pedrocornejocarvajal <75272975+pedrocornejocarvajal@users.noreply.github.com>
2022-11-18 11:52:13 +0100

    Changes by jgarcia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.sound.midi.Soundbank;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,19 +9,58 @@ public class Main {
     private static final String password = "1234";
     public static Statement statement = null;
     public static Connection connection = crearConexion();
+    public static Scanner sc;
 
     public static void main(String[] args) {
+        sc = new Scanner(System.in);
         try {
             statement = connection.createStatement();
             iniciarSesion();
+//            switch (menu()) {
+//                case 1 -> iniciarSesion();
+//                case 2 -> crearUsuario();
+//            }
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        sc.close();
     }
 
+    private static void crearUsuario() {
+    }
+
+//    private static int menu() {
+//    }
+
     private static void iniciarSesion() {
-        
+        String nombre, contrasenia;
+        System.out.println("Inicio de Sesion");
+        System.out.println("Introduce tu usuario");
+        nombre = sc.nextLine();
+        System.out.println("Introduce tu constraseña");
+        contrasenia = sc.nextLine();
+        if (conIniciarSesion(nombre,contrasenia)){
+            System.out.println("Iniciado");
+            //lee el mensaje de arriba
+        } else {
+            System.out.println("No iniciado");
+        }
+    }
+
+    private static boolean conIniciarSesion(String nombre, String contrasenia) {
+        boolean isIniciado = false;
+        try{
+            ResultSet rs = statement.executeQuery("SELECT idUsuario FROM ad2223_jgarcia.Usuarios WHERE Nombre LIKE ('"+nombre+"') and Contrasenia like ('"+contrasenia+"');");
+            while(rs.next()){
+                if(rs.getInt("idUsuario") != 0){
+                    isIniciado = true;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return isIniciado;
     }
 
     /**
